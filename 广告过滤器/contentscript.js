0a87789f7b5bea3222473178b072a907035e15c8
FourLeafClover <zc_smile@outlook.com>
2017-04-06 10:18:55 +0800

    广告过滤优化

--- 广告过滤器/contentscript.js
+++ 广告过滤器/contentscript.js
@@ -1,11 +1,30 @@
 var removeAD = function() {
     if ($) {
+        var isRemoveAD = false;
         $("div").each(function() {
+            var ele = $(this);
             var cl = $.trim($(this).attr("class"));
-            if (cl.length == 41) {
-                $(this).hide();
-            }
-        })
+            cl.split(' ').forEach(function(c) {
+                // 广告的class一般用的uuid字符串.
+                if (c.length >= 36) {
+                    ele.remove();
+                    window.IsHaveAD = true;
+                    window.ADLog.push(c);
+                    isRemoveAD = true;
+                }
+            })
+        });
+        if (isRemoveAD) {
+            clearInterval(removeADSetInter);
+            console.log("拦截了" + window.ADLog.length + "个广告");
+        }
     }
 }
-setInterval(removeAD, 50);
+
+window.ADLog = [];
+window.IsHaveAD = false;
+var removeADSetInter = setInterval(removeAD, 50);
+setTimeout(function() {
+    clearInterval(removeADSetInter);
+
+}, 10000);
